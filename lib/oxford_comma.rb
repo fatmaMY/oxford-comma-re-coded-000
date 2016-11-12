e7f433251386c04b76010318f2db0e31cf9dc38c
fatma yousef <famyaltimimi@yahoo.com>
2016-11-12 10:31:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.size>2
+    last = array.pop
+  array.join(", ") + ", and " + last.to_s
+elsif array.size==2
+    array.join(" and ")
+  #elsif array.size==3
+      #array.insert(2,"and ")
+      #array.insert(1,", ")
+      #array.insert(3,", ")
+      #array.join("")
+    elsif
+      array.size==1
+      array.join(" ")
+  end
 end
